38fadeec0396c4fee217af71fcb0967e57488118
Apoorv Goyal <apoorvelous@gmail.com>
2021-02-23 10:19:29 +0530

    revamped

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,3 @@
-// import React, { Component } from 'react';
-// import { Text, View } from 'react-native';
-
-// export default class HomeScreen extends Component {
-//     render() {
-//         return (
-//             <View
-//                 style={{
-//                     flex: 1,
-//                     justifyContent: "center",
-//                     alignItems: "center"
-//                 }}>
-//                 <Text>Home Screen!</Text>
-//             </View>
-//         )
-//     }
-// }
-
 import React, { Component } from "react";
 import {
     View,
@@ -30,37 +12,38 @@ import {
 } from "react-native";
 
 export default class HomeScreen extends Component {
-    
+
 
     render() {
         return (
             <View style={styles.container}>
-                 <SafeAreaView style={styles.droidSafeArea} />
+                <SafeAreaView style={styles.droidSafeArea} />
                 <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
                     <View style={styles.titleBar}>
-                        <Image source={require("../assets/main-icon.png")} style={{width:100, height:100}}></Image>
-                        <Text style={styles.titleText}>Stellar App</Text>
+                        <Image source={require("../assets/main-icon.png")} style={{ width: 150, height: 150 }}></Image>
+                        <Text style={styles.titleText}>Stellar</Text>
+                        <Text style={styles.titleText}>App</Text>
                     </View>
-                  
+
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("SpaceCrafts")
                     }>
                         <Text style={styles.routeText}>Space Crafts</Text>
-                        <Image source={require("../assets/asset-06.png")} style={{ width: 80, height: 80 }}></Image>
+                        <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
-                  
+
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("StarMap")
                     }>
                         <Text style={styles.routeText}>Star Map</Text>
-                        <Image source={require("../assets/star-map.png")} style={{ width: 80, height: 80 }}></Image>
+                        <Image source={require("../assets/star_map.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
-                  
+
                     <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("DailyPic")
                     }>
                         <Text style={styles.routeText}>Daily Pictures</Text>
-                        <Image source={require("../assets/photo.png")} style={{ width: 80, height: 80 }}></Image>
+                        <Image source={require("../assets/daily_pictures.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
                 </ImageBackground>
             </View>
@@ -80,30 +63,38 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
     },
     routeCard: {
-        flex: 0.2,
+        flex: 0.12,
         justifyContent: "center",
         alignItems: "center",
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 5,
-        borderRadius: 10,
-        backgroundColor: 'rgba(52, 52, 52, 0.5)'
-        // backgroundColor:'white'
+        margin: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        borderRadius: 100,
+        backgroundColor: "white"
     },
     titleBar: {
-        flex: 0.25,
+        flex: 0.5,
         justifyContent: "center",
         alignItems: "center"
     },
     titleText: {
-        fontSize: 30,
+        fontSize: 40,
         fontWeight: "bold",
         color: "white"
     },
     routeText: {
         fontSize: 25,
         fontWeight: "bold",
-        // color: "white"
-        color:'#D11583'
+        color: '#D11583',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    routeImage: {
+        position: "absolute",
+        top: -20,
+        right: -15,
+        height: 80,
+        width: 80,
+        resizeMode: "contain"
     }
 });
--- screens/SpaceCrafts.js
+++ screens/SpaceCrafts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View ,Alert,FlatList,Image,ImageBackground,StyleSheet, SafeAreaView,Platform, StatusBar} from 'react-native';
+import { Text, View, Alert, FlatList, Image, ImageBackground, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 import axios from 'axios';
 
@@ -11,75 +11,76 @@ export default class SpaceCraftsScreen extends Component {
         };
     }
 
-componentDidMount(){
-    this.getData()
-    
-}
+    componentDidMount() {
+        this.getData()
 
-getData=()=>{
-    axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
+    }
+
+    getData = () => {
+        axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
             .then(response => {
-                
+
                 this.setState({ aircrafts: response.data.results })
-                
+
             })
             .catch(error => {
                 console.log(error.message)
             })
-    
-}
+
+    }
 
 
 
 
-renderItem = ({item}) =>{
-    return(
-        <View style={styles.contentCard}>
-            <Image
-            source={{uri: item.agency.image_url}} style={styles.itemImage}></Image>
-        <Text style={{fontWeight:'bold', fontSize:20,color:'purple'}}>{item.name}</Text>
-        <Text style={{color:'#696969'}}>{item.agency.name}</Text>
-        <Text>DESCRIPTION</Text>
-        <Text style={{color:'#A9A9A9',marginLeft:10,marginRight:10}}>{item.agency.description}</Text>
-        </View>
-    )
-}
-
-keyExtractor = (item, index) => index.toString();
-
-    render() {
-        console.log(this.state.aircrafts.length)
-        if (Object.keys(this.state.aircrafts).length === 0) {
-            return (
-                <View
-                    style={{flex: 1,justifyContent: "center",alignItems: "center"}}>
-                    <Text>Loading</Text>
-                </View>
-            )
-        } else{
+    renderItem = ({ item }) => {
         return (
-            <View style={styles.container}>
-                <SafeAreaView style={styles.droidSafeArea}/>
-                <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
-                <View style={{flex:0.10,alignItems:'center',justifyContent:'center'}}>
-                    <Text style={styles.titleText}>Space Crafts</Text>
+            <View style={styles.contentCard}>
+                <Image source={{ uri: item.agency.image_url }} style={styles.itemImage}></Image>
+                <View style={{ padding: 20 }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'purple' }}>{item.name}</Text>
+                    <Text style={{ color: '#696969', fontSize: 16 }}>{item.agency.name}</Text>
+                    <View style={{ marginTop: 10 }}>
+                        <Text style={{ color: '#A9A9A9', fontSize: 13 }}>{item.agency.description}</Text>
+                    </View>
                 </View>
-                <View style={{flex:0.90}}>
-                     <FlatList
-                    keyExtractor={this.keyExtractor}
-                    data={this.state.aircrafts}
-                    renderItem={this.renderItem}
-                    initialNumToRender={10}
-                />
-                </View>
-                
-               
-               
-                </ImageBackground>
             </View>
         )
     }
-}
+
+    keyExtractor = (item, index) => index.toString();
+
+    render() {
+        if (Object.keys(this.state.aircrafts).length === 0) {
+            return (
+                <View
+                    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text>Loading</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
+                        <View style={{ flex: 0.15, alignItems: 'center', justifyContent: 'center' }}>
+                            <Text style={styles.titleText}>Space Crafts</Text>
+                        </View>
+                        <View style={{ flex: 0.85 }}>
+                            <FlatList
+                                keyExtractor={this.keyExtractor}
+                                data={this.state.aircrafts}
+                                renderItem={this.renderItem}
+                                initialNumToRender={10}
+                            />
+                        </View>
+
+
+
+                    </ImageBackground>
+                </View>
+            )
+        }
+    }
 }
 
 const styles = StyleSheet.create({
@@ -93,28 +94,27 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode: 'cover',
     },
-    titleText:{
-        fontSize: 25,
+    titleText: {
+        fontSize: 35,
         fontWeight: "bold",
         color: "white",
-        justifyContent:"center",
-        alignContent:"center",     
+        justifyContent: "center",
+        alignContent: "center",
     },
-    contentCard:{
-        borderRadius:5,
-        justifyContent:'center',
-        alignItems:'center',
-        marginBottom:10,
-        elevation:10,
-        backgroundColor: 'rgba(52, 52, 52, 0.5)'
+    contentCard: {
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20,
+        elevation: 10,
+        backgroundColor: 'white'
     },
-    itemImage:{
+    itemImage: {
         width: "100%",
         height: 200,
-        marginTop:1,
-        marginBottom:15,
-        marginLeft:10,
-        marginRight:10,
-        borderRadius:5
+        marginBottom: 15,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 5
     }
 })
